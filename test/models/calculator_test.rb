4014613c7bd3beb43d287399b6e98ceda8054f24
Richard Temple <richardtemple@mutexmusic.com>
2016-02-05 19:25:47 -0800

    added more tests... need fix for nil yearly_retirement_income

--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -21,4 +21,23 @@ class CalculatorTest < ActiveSupport::TestCase
     calc = Calculator.new
     assert (calc.current_savings > 0), "current_savings should have default value."
   end
+
+  test "shouldn't return an ARI if the calculating values are invalid" do
+    calc = Calculator.new
+    calc.current_savings = "abcd"
+    assert calc.yearly_retirement_income == nil, "Calculator output should be nil still"
+    calc = calc.update 
+    # assert calc.yearly_retirement_income == false,
+    #       "Expected: error. Actual: #{calc.yearly_retirement_income} "
+  end
+
+  test "should correctly calculate the ARI in today's dollars" do
+    calc = Calculator.new
+    calc.show_in_todays_dollars = "1"
+    assert calc.yearly_retirement_income == nil, "Calculator output should be nil still"
+    calc = calc.update 
+    assert calc.yearly_retirement_income == 28783.9781817964, 
+          "Expected: 28783.9781817964. Actual: #{calc.yearly_retirement_income} "
+  end
+
 end
